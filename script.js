bb2fe09a68a43878ccbaea8cd3c8ba275a88b099
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-07-09 15:31:09 -0600

    Changed questions into an array of objects

--- script.js
+++ script.js
@@ -9,19 +9,39 @@ var userAnswers = [];
 var correctAnswers = [];
 var wrongAnswers = [];
 
-//"What does a .querySelector do?":[2] or b is the correct answer
-var questionOneAnswers = {
-    Question1: "What does a .querySelector do?",
-    a: "Selects a question on the page", 
-    b: "Selects a specific element of an html file",
-    c: "Selects every question on an html file",
-    d: "Selects a specific element of a Javascript file"
-};
+//"What does a .querySelector do?":[1] or b is the correct answer
+var allQuestions = [ 
+    {
+        Question1: "What does a .querySelector do?",
+        firstQChoices: [ "Selects a question on the page", "Selects a specific element of an html file", "Selects every question on an html file", "Selects a specific element of a Javascript file"],
+        firstQAnswer: "Selects a specific element of an html file"
+    },
+    {  
+        Question2: "What method would you use to mix around the output of contents in an array?",
+        secondQChoices: ["Math.mix", "randomInt", "randomArray", "Math.random"],
+        secondQAnswer: "Math.random"
+    },
+    {
+        Question3: "What is a DOM?",
+        thirdQChoices: ["Document Object Module", "Document Options & Materials", "Document Object Model", "Document Orientation Mender"],
+        thirdQAnswer: "Document Object Model"
+    },
+    {
+        Question4: "What does an .addEventListener do?",
+        fourthQChoices: ["Runs a function or specified change to the page whenever an event is triggered by the user", "User's computer microphone listens for a command to run a function", "Runs a function every time a user listens to music on their computer", "Runs a function every time a user adds an event to their dateCalender"],
+        fourthQAnswer: "Runs a function or specified change to the page whenever an event is triggered by the user"
+    },
+    {
+        Question5: "What method would you use to change the appearance of an element without using the css file?",
+        fifthQChoices: [".setAttribute", ".changeElement", "setElement", ".changeAttribute"],
+        fifthQAnswer: ".setAttribute"
+    }
+];
 
 var questionOne = function() {
-    if (userAnswers === questionOneAnswers[2]) {
+    if (userAnswers === allQuestions[0].firstQAnswer) {
         userAnswers = userAnswers.concat(correctAnswers)
-    } else if (userAnswers !== questionOneAnswers[2]) {
+    } else if (userAnswers !== allQuestions[0].firstQAnswer) {
         userAnswers = userAnswers.concat(wrongAnswers)
         //wrongAnswerTimer()
     }
@@ -35,23 +55,8 @@ var firstQ = function() {
 }
 startBtn.addEventListener("click", firstQ);
 
-
-/*do i need to add the questionOneAnswers object/array to the event listener to get it to display 
-when user clicks the start button? */
-//startBtn.addEventListener("click", firstQ());
-
-
 //nextQuestion.addEventListener("click", /*function here*/);
 //previousQuestion.addEventListener("click", /*function here*/);
-
-//"What method would you use to mix around the output of contents in an array?"[4] or d is the correct answer
-var questionTwoAnswers = {
-    "Question #2": "What method would you use to mix around the output of contents in an array?",
-    "a": "Math.mix",
-    "b": "randomInt",
-    "c": "randomArray",
-    "d": "Math.random"
-};
 
 var questionTwo = function() {
     if (userAnswers === questionTwoAnswers[4]) {
@@ -62,15 +67,6 @@ var questionTwo = function() {
     }
 }
 
-//"What is a DOM?"[3] or c is the correct answer
-var questionThreeAnswers = {
-    "Question #3": "What is a DOM?",
-    "a": "Document Object Module",
-    "b": "Document Options & Materials",
-    "c": "Document Object Model",
-    "d": "Document Orientation Mender"
-};
-
 var questionThree = function() {
     if (userAnswers === questionThreeAnswers[3]) {
         userAnswers = userAnswers.concat(correctAnswers)
@@ -80,15 +76,6 @@ var questionThree = function() {
     }
 }
 
-//"What does an .addEventListener do?"[1] or a is the correct answer
-var questionFourAnswers = {
-    "Question #4": "What does an .addEventListener do?",
-    "a": "Runs a function or specified change to the page whenever an event is triggered by the user",
-    "b": "User's computer microphone listens for a command to run a function",
-    "c": "Runs a function every time a user listens to music on their computer",
-    "d": "Runs a function every time a user adds an event to their dateCalender"
-};
-
 var questionFour = function() {
     if (userAnswers === questionFourAnswers[1]) {
         userAnswers = userAnswers.concat(correctAnswers)
@@ -96,15 +83,6 @@ var questionFour = function() {
         userAnswers = userAnswers.concat(wrongAnswers)
         //wrongAnswerTimer()
     }
-}
-
-//"What method would you use to change the appearance of an element without using the css file?"[1] or a is correct answer
-var questionFiveAnswers = {
-    "Question #5": "What method would you use to change the appearance of an element without using the css file?",
-    "a": ".setAttribute",
-    "b": ".changeElement",
-    "c": "setElement",
-    "d": ".changeAttribute"
 }
 
 var questionFive = function() {
@@ -127,19 +105,6 @@ var questionFive = function() {
         window.alert("you got " + wrongAnswers + " wrong.")
 }*/
 
-//todo object with functions/methods inside it for questions instead of separate functions?
-//const quizQuestions = {
-/*var allQuestions = [
-    {
-        prompt: "What does a .querySelector do?",
-        answer: ["a: Selects a question on the page", 
-        "b: Selects a specific element of an html file",
-        "c: Selects every question on an html file",
-        "d: Selects a specific element of a Javascript file",[1]]
-    }
-]*/
-
-//todo create an object with ALL questions and array the answers and [index] the correct one?
 //todo add this to every question? or add a i-- ??
 /*var quizTimer = 
 
